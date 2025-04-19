f454835b913893822a22461bbee981be4f406337
Seraphrai <84433119+Eragonlink@users.noreply.github.com>
2025-04-19 13:38:56 -0500

    Init db connection

--- frontend.py
+++ frontend.py
@@ -1,5 +1,16 @@
 import pymongo
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-mydb = myclient["mydatabase"]
+uri = "mongodb+srv://Michael:root@cluster0.gn5rqjv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
