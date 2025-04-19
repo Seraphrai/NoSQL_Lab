a1e4624393af4f1fe66140b71c3aae64ec7494c1
Seraphrai <84433119+Eragonlink@users.noreply.github.com>
2025-04-19 13:54:38 -0500

    Flask integrated

--- backend.py
+++ backend.py
@@ -1,5 +1,6 @@
-import pymongo
-
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
@@ -8,6 +9,18 @@ uri = "mongodb+srv://Michael:root@cluster0.gn5rqjv.mongodb.net/?retryWrites=true
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
+app = Flask(__name__)
+
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
+@app.route('/hello')
+def hello_world():
+    return 'Hello World'
+
+@app.route('/ping')
 def pingServer ():
     # Send a ping to confirm a successful connection
     try:
@@ -16,3 +29,10 @@ def pingServer ():
     except Exception as e:
         print(e)
 
+
+# main driver function
+if __name__ == '__main__':
+
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run()
