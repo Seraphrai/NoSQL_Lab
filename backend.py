c03aacfd3e7955733efcecb609b010d93132dda4
michaelh4 <michaelhurtado440@gmail.com>
2025-04-27 19:41:34 -0500

    Added updated vote

--- backend.py
+++ backend.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from flask_cors import CORS
-import datetime
+from datetime import datetime
 uri = "mongodb+srv://Michael:root@cluster0.gn5rqjv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
@@ -14,6 +14,15 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 # current module (__name__) as argument.
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/retrieveBallot')
+def retrieveBallot():
+    data = request.get_json()
+    db = client["NoSQL_Lab"]
+    votes = db["Votes"]
+    voterID = data.get("voterID")  
+
+    return votes.find({"voter.voterID": voterID})
 
 @app.route('/deleteBallot', methods=["POST"])
 def deleteBallot():
@@ -42,10 +51,10 @@ def processVote():
 
     voterID = data.get("voterID")
     regPIN = data.get("regPIN")
-    electionID = data.get("election")
+    electionID = data.get("electionID")
     electionName = data.get("electionName")
-    electionDate = str(datetime.date.today())
-    timestamp = str(datetime.now().timestamp())
+    electionDate = str(datetime.date(datetime.now()))
+    timestamp = str(datetime.now())
     candidate1ID = data.get("candidate1ID")
     candidate2ID = data.get("candidate2ID")
     candidate3ID = data.get("candidate3ID")
@@ -72,13 +81,57 @@ def processVote():
         
         "rankings": [
             {"rank": 1, "nominee": { "nomineeID": candidate1ID, "name": candidate1Name, "party": candidate1Party}},
-            {"rank": 1, "nominee": { "nomineeID": candidate2ID, "name": candidate2Name, "party": candidate2Party}},
-            {"rank": 1, "nominee": { "nomineeID": candidate3ID, "name": candidate3Name, "party": candidate3Party}}
+            {"rank": 2, "nominee": { "nomineeID": candidate2ID, "name": candidate2Name, "party": candidate2Party}},
+            {"rank": 3, "nominee": { "nomineeID": candidate3ID, "name": candidate3Name, "party": candidate3Party}}
         ]
     }
 
     result = votes.insert_one(ballot)
     
+    return str(result)
+
+@app.route('/updateVote', methods=["POST"])
+def updateVote():
+
+    data = request.get_json()
+    db = client["NoSQL_Lab"]
+    votes = db["Votes"]
+
+    voterID = data.get("voterID")
+    regPIN = data.get("regPIN")
+    electionID = data.get("electionID")
+    electionName = data.get("electionName")
+    electionDate = str(datetime.date(datetime.now()))
+    timestamp = str(datetime.now())
+    candidate1ID = data.get("candidate1ID")
+    candidate2ID = data.get("candidate2ID")
+    candidate3ID = data.get("candidate3ID")
+    candidate1Name = data.get("candidate1Name")
+    candidate2Name = data.get("candidate2Name")
+    candidate3Name = data.get("candidate3Name")
+    candidate1Party = data.get("candidate1Party")
+    candidate2Party = data.get("candidate2Party")
+    candidate3Party = data.get("candidate3Party")
+
+    filterVote = {"voter.voterID": voterID, "voter.regPIN": regPIN}
+    newValues = {
+        "election": {
+            "electionID": electionID,
+            "name": electionName, 
+            "date": electionDate
+        },
+
+        "timestamp": timestamp,
+        
+        "rankings": [
+            {"rank": 1, "nominee": { "nomineeID": candidate1ID, "name": candidate1Name, "party": candidate1Party}},
+            {"rank": 2, "nominee": { "nomineeID": candidate2ID, "name": candidate2Name, "party": candidate2Party}},
+            {"rank": 3, "nominee": { "nomineeID": candidate3ID, "name": candidate3Name, "party": candidate3Party}}
+        ]
+    }
+
+    #result = votes.update_one(filterVote, {"$set": {"election": {"electionID": electionID,"name": electionName, "date": electionDate}}})
+    result = votes.update_one(filterVote, {"$set": newValues})
     return str(result)
 
 @app.route('/checkDuplicate', methods=["POST"])
@@ -88,7 +141,7 @@ def checkDuplicate ():
 
     data = request.get_json()
 
-    myquery = { "voterID": data.get("voterID"), "regPIN": data.get("regPIN") }
+    myquery = { "voter.voterID": data.get("voterID"), "voter.regPIN": data.get("regPIN") }
 
     if votes.find_one(myquery) is None:
         print("False")
